6db29fd4774b60d99bbcabfcddabbd71ea2309c8
gowtham <gowthamsgobi@gmail.com>
2023-07-20 09:55:24 +0530

    project was created

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// -- stylesheet imported here
+import './App.css';
+
+// -- App component
+const App = () => {
+  return(
+    <>
+    <h1>React Typescript 25 concepts App</h1>
+    </>
+  )
+}
+
+export default App;
